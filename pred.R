1562501a90eb84ea94cd2bb593eef2041bd68c8a
Ben Bolker <bbolker@gmail.com>
2019-10-24 22:45:13 -0400

    braindump edits

--- pred.R
+++ pred.R
@@ -18,21 +18,13 @@ m3 <- glmer(use ~ age * ch + I(age^2) + urban + (1 | urban:district),
 
 ## effects
 
-zeroInt <- function(term,...) {
-    ret_fun <- function(m,...) {
-        browser()
-        tt <- terms(m)
-        ## columns or rows?
-        ff <- attr(tt,"factors")[term,]
-        ## ff <- ff[-attr(tt,"response")] ## drop response variable
-        if (attr(tt,"intercept")==1) ff <- c(0,ff)
-        v <- vcov(m)*outer(ff,ff)
-    }
-    return(ret_fun)
+zeroInt <- function(m,...) {
+    v <- vcov(m)
+    v["(Intercept)",] <-  v[,"(Intercept)"] <- 0
+    return(v)
 }
 ## doesn't really work
-plot(allEffects(m3,vcov. = zeroInt("age")))
-plot(Effect("age",m3,vcov. = zeroInt("age")))
+plot(allEffects(m3,vcov. = zeroInt))
 
 plot(allEffects(m3))
 
@@ -44,16 +36,18 @@ plot(predictorEffects(m3,partial.residuals=TRUE),
 
 dd <- as.data.frame(allEffects(m3,
             xlevels=list(age=seq(-10,20,length=51))))
-
+View(dd[["age:ch"]])
 
 
 ggplot(dd[["age:ch"]],aes(age,fit))+
     geom_line()+
     facet_wrap(~ch)+
-    geom_ribbon(aes(ymin=lower,ymax=upper),colour=NA,alpha=0.2)+
-    scale_y_continuous(trans=scales::logit_trans(),
+    geom_ribbon(aes(ymin=lower,ymax=upper),
+                colour=NA,alpha=0.2)+
+  scale_y_continuous(trans=scales::logit_trans(),
                        breaks=c(0.01,0.05,0.1,0.2,0.4,0.6))+
-    labs(x="centred age",y="probability of contraception use")
+    labs(x="centred age (years)",
+         y="probability of contraception use")
 
 
 ## sjPlot
