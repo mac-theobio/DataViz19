cda0642c3246e5dfe23154c57eafce8b6dbe30e8
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-11-15 09:10:40 -0500

    Autocommit (public)

--- media.R
+++ media.R
@@ -1,4 +1,14 @@
 library(igraph)
 library(ggraph)
-nodes <- read.csv("sunbelt/Dataset1-Media-Example-NODES.csv", header=T, as.is=T)
-links <- read.csv("sunbelt/Dataset1-Media-Example-EDGES.csv", header=T, as.is=T)
+
+## Put Data directory from http://www.kateto.net/wordpress/wp-content/uploads/2019/06/sunbelt2019.zip here as sunbelt
+ntmp <- read.csv("sunbelt/Dataset1-Media-Example-NODES.csv", header=TRUE, as.is=TRUE)
+ltmp <- read.csv("sunbelt/Dataset1-Media-Example-EDGES.csv", header=TRUE, as.is=TRUE)
+
+medialinks <- graph_from_data_frame(d=ltmp, vertices=ntmp, directed=TRUE) 
+
+plot(medialinks)
+plot(medialinks, edge.arrow.size=.4)
+plot(medialinks, edge.arrow.size=.4, layout=layout_randomly)
+plot(medialinks, edge.arrow.size=.4,vertex.label=NA)
+
