6957daacd13fca72e640ab2bdb10fe748f1950f7
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-11-15 11:19:31 -0500

    During class

--- ggmedia.R
+++ ggmedia.R
@@ -1,14 +1,21 @@
 library(igraph)
 library(ggraph)
-theme_set(theme_bw())
+theme_set(theme_void())
 
 layout <- ggraph(medialinks, layout="fr")
 
-print(layout
+old <- print(layout
 	+ geom_edge_link()
 	+ geom_node_point()
 	+ geom_node_text(aes(label=media))
 ) 
 
-## from,to,type,weight
-## id,media,media.type,type.label,audience.size
+## EDGE aesthetics: type,weight
+## NODE media,media.type,type.label,audience.size
+
+print(layout
+	+ geom_edge_fan(aes(color=type, width=weight))
+	+ geom_node_point(aes(size=audience.size, color=type.label))
+	+ scale_size_area() ## Confirm DOES THIS WORK?
+	+ geom_node_text(aes(label=media), repel=TRUE)
+) 
