09d2958b5ea864a3f492a8e25158b6e6372d1154
Ben Bolker <bbolker@gmail.com>
2021-05-20 10:36:26 -0400

    Orphan commit!

--- pkgs.R
+++ pkgs.R
@@ -3,3 +3,5 @@ cividis https://github.com/marcosci/cividis
 pheatmap
 andrews
 pgmm
+rggobi (archive, version 2.1.22) [libgtk22.0-dev] [apt install ggobi]
+devtools::install_version("rggobi",version="2.1.22")
